1b4e318f813a1fbc979263af68d9d6286c565e27
Aman Shukla <amanks7400@gmail.com>
2022-08-22 15:38:32 +0530

    Add Powerups Logo

--- pages/offers.js
+++ pages/offers.js
@@ -18,6 +18,13 @@ export default function Offers() {
     )
 }
 
+const powerups = [
+    {companyName: "Pizza Hut", logo:"/ph-logo.png",},
+    {companyName: "Urban Company", logo:"/uc-logo.png"},
+    {companyName: "Burger King", logo:"/bk-logo.png"},
+    {companyName: "Starbucks", logo:"/sb-logo.png"},
+]
+
 function Powerups() {
     return (
         <>
@@ -26,23 +33,23 @@ function Powerups() {
             </Heading>
             <HStack my={8} gap={4}>
                 {
-                    [1, 2, 3, 4].map((item, i) =>
+                    powerups.map((item, i) =>
                         <Box cursor="pointer" key={i} p={4} borderRadius="8" boxShadow="md">
                             {/* Company Name */}
                             <Heading size="md">
-                                Brand {item}
+                                {item.companyName}
                             </Heading>
                             {/* Powerup Name */}
                             <Text my={4} fontSize="2xl">
-                                Get {5 * item}% cashback
+                                Get {5 * (i+1)}% cashback
                             </Text>
                             <HStack justifyContent="space-between">
                                 {/* time left */}
                                 <HStack>
-                                    <AiOutlineClockCircle />   <Text> {item} days left</Text>
+                                    <AiOutlineClockCircle />   <Text> {i+1} days left</Text>
                                 </HStack>
                                 {/* Company logo */}
-                                <img src="/master-card.png" width={64} height={64} alt={`Brand ${item}`} />
+                                <img src={item.logo} width={48} height={48} alt={`Brand ${item.companyName}`} />
                             </HStack>
 
                         </Box>)
